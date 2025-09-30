81851a92f3c45d1d93085951505edb4ec09c1909
LUCAS RAFAEL PEREIRA <lucas_pereira25@estudante.sesisenai.org.br>
2025-09-30 12:44:56 -0300

    Conectamos o wifi

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,8 +1,17 @@
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
 void setup() {
-  // put your setup code here, to run once:
-
+Serial.begin(115200);
+WiFi.begin(SSID, PASS);
+Serial.println("Conectando no WiFi");
+while (WiFi.status()!= WL_CONNECTED){
+Serial.print(".");
+delay(200);
+  }
+Serial.print("Conectado com sucesso!");
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 
